47bb01993dc35fb9f826765e8546bb687a2d9045
Gandhi-Rajendran <ksrmgandhiraj@gmail.com>
2022-09-22 21:08:46 +0530

    Add modal and Icon modal for Tab view implemented in all pages.

--- src/views/md-details/index.tsx
+++ src/views/md-details/index.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 import TablePageHeader from "../../components/common-table-page-header";
+import AddMdDetailsModal from "../../components/modals/add-md-details-modal";
 import MdDetailsTable from "../../components/tables/md-details-table";
+
 import S from "./mdDetails.styled";
 
 const MdDetails = () => {
+  const [addMdDetails, setAddMdDetails] = useState(false);
+
+  const addMdDetailsModalHandler = () => {
+    setAddMdDetails(!addMdDetails);
+  };
+
   return (
     <S.MdDetailsContainer>
-      <TablePageHeader />
+      <TablePageHeader addMdDetailsModalHandler={addMdDetailsModalHandler} />
       <MdDetailsTable />
+      <AddMdDetailsModal openModal={addMdDetails} handleClose={addMdDetailsModalHandler} />
     </S.MdDetailsContainer>
   );
 };
