38257c8c1879f3ab3af91b26397f486e0b38a155
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-09-24 16:18:57 +0530

    Fixed the App Component.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -7,18 +7,17 @@ import Login from "../views/login";
 import "froala-editor/js/froala_editor.pkgd.min.js";
 import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
-import ModalLaunchButtons from "../components/modals/ModalLaunchButtons";
 
 const App: FC = () => {
   return (
     <>
       <Provider>
-        {/* <Routes>
+        <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<Layout />} />
-        </Routes> */}
-        <ModalLaunchButtons/>
+        </Routes>
       </Provider>
+
     </>
   );
 };
