b5d83eb3deb7f09b387d4c7afcf68d9d44fa9969
tselvan-aeq <thamarai@aequalisys.com>
2022-09-28 18:38:15 +0530

    Icons file name changed.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
-import './assets/css/icon.css';
+import "./assets/css/nerkathir-icons.css";
 import "./assets/css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
