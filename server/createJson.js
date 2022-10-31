10f35f71212453100452b3e778b03fc023be47bd
tselvan-aeq <thamarai@aequalisys.com>
2022-10-31 12:03:15 +0530

    Server File created for json-server.

---
+++ server/createJson.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const ceoFile = require('./src/ceo.json');
+const farmerDetails = require('./src/farmerDetails.json');
+const farmerGroup = require('./src/farmerGroup.json');
+const founders = require('./src/founders.json');
+const resolutions = require('./src/resolutions.json')
+
+const handleJSONFile = ()=>{
+    const key = ['ceo','farmerDetails','farmerGroup','founders','resolutions']
+    const values = [ceoFile,farmerDetails,farmerGroup,founders,resolutions];
+    
+    const file = {};
+
+    key.map((item,index)=>{
+        file[item] = values[index]
+    });
+    fs.writeFile('db.json',JSON.stringify(file),err=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+    })
+  }
+
+  handleJSONFile();
