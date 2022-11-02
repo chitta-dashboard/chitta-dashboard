95b39078a694857fd2d5d30f9bea8dccd002e6a4
vinothkumar-aeq <vinothkumar@aequalisys.com>
2022-11-02 11:31:51 +0530

    Converted resolution state management from context to @reduxjs/toolkit.

--- src/components/resolutions/resolutions-header/index.tsx
+++ src/components/resolutions/resolutions-header/index.tsx
@@ -1,32 +1,29 @@
 import { FC, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import IconWrapper from "../../../utils/iconWrapper";
-import { useResolutionsProviderContext } from "../../../utils/context/resolutions";
 import ResolutionModal from "../../modals/resolution-modal";
+import { addResolution, changeTab } from "../../../utils/store/slice/resolution";
+import { RootState } from "../../../utils/store";
 import S from "./resolutionsHeader.styled";
 
-interface CustomProps {
-  viewTree(): void;
-  viewList(): void;
-  treeView: boolean;
-}
-
-const ResolutionsHeader: FC<CustomProps> = ({ viewTree, viewList, treeView }) => {
+const ResolutionsHeader: FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const { resolutions, addResolution } = useResolutionsProviderContext();
+  const dispatch = useDispatch();
+  const { resolutions, tab } = useSelector((state: RootState) => state.resolution);
 
   return (
     <>
       <S.Header>
-        <IconWrapper onClick={viewTree} isDummy={treeView}>
+        <IconWrapper onClick={() => dispatch(changeTab("tree"))} isDummy={tab === "tree"}>
           back
         </IconWrapper>
         <S.Title>Board Resolution</S.Title>
         <S.ButtonBox>
-          {Object.values(resolutions).length > 4 && treeView ? <S.Button onClick={viewList}>View All</S.Button> : null}
+          {Object.values(resolutions).length > 4 && tab === "tree" ? <S.Button onClick={() => dispatch(changeTab("list"))}>View All</S.Button> : null}
           <S.Button onClick={() => setModalOpen(true)}>Add</S.Button>
         </S.ButtonBox>
       </S.Header>
-      {modalOpen && <ResolutionModal openModal={true} handleClose={() => setModalOpen(false)} cb={addResolution} />}
+      {modalOpen && <ResolutionModal openModal={true} handleClose={() => setModalOpen(false)} cb={(data) => dispatch(addResolution(data))} />}
     </>
   );
 };
