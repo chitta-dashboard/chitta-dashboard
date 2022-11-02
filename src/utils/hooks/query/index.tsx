bd17764f56eee52adae3026fa426dad536d80bad
adurhmn-aeq <abdur@aequalisys.com>
2022-11-02 18:32:38 +0530

    Query structure improved.

--- src/utils/hooks/query/index.tsx
+++ src/utils/hooks/query/index.tsx
@@ -1,29 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { queryClient } from "../../../containers/provider";
-import { groupBy } from "../../constants";
+import { Endpoints, groupBy } from "../../constants";
 
-export const useFetchResolutions = () => {
+export const useFetch = (endpoint: Endpoints) => {
   const [formatChangeSuccess, setformatChangeSucess] = useState(false);
 
   const result = useQuery({
-    queryKey: ["resolutions-fetch"],
+    queryKey: [`${endpoint}-fetch`],
     queryFn: () => {
-      console.log("[resolutions-fetch] request executed");
-      return fetch("http://localhost:5001/resolutions").then((res) => res.json());
+      console.log(`[${endpoint}-fetch] request executed`);
+      return fetch(`http://localhost:5001/${endpoint}`).then((res) => res.json());
     },
-    cacheTime: Infinity,
-    staleTime: Infinity,
+    cacheTime: Infinity, // do not change!
+    staleTime: Infinity, // do not change!
   });
 
   useEffect(() => {
-    if (result.data !== undefined && result.isFetching) {
-      queryClient.setQueryData(["resolutions-fetch"], groupBy(result.data, "id"));
+    if (result.data !== undefined && result.isFetched) {
+      queryClient.setQueryData([`${endpoint}-fetch`], groupBy(result.data, "id"));
       setformatChangeSucess(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [result.isFetching]);
+  }, [result.isFetched]);
 
   return { formatChangeSuccess, result };
 };
-useFetchResolutions.queryKey = ["resolutions-fetch"];
