02bc2901cb81228bd6bc8994f6b27931b7ec47d7
raviprasad-aeq <raviprasad@aequalisys.com>
2022-11-08 17:53:33 +0530

    Share holder certificate, export farmers will now generate according to sort and search, create json functionality improved.

--- src/components/tables/farmers-details-table/body/index.tsx
+++ src/components/tables/farmers-details-table/body/index.tsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ENDPOINTS, handleDataByPage, searchWord, sortObj } from "../../../../utils/constants";
-import { addFarmerDetails, farmerDetail, addFarmerId, setPageCount, checkBoxUnselectAll,setFarmersIdToExport } from "../../../../utils/store/slice/farmerDetails";
+import {
+  addFarmerDetails,
+  farmerDetail,
+  addFarmerId,
+  setPageCount,
+  checkBoxUnselectAll,
+  setFarmersIdToExport,
+} from "../../../../utils/store/slice/farmerDetails";
 import Loader from "../../../loader";
 import { useFetch } from "../../../../utils/hooks/query";
 import BodyWrapper from "../../../custom-tables/body";
@@ -12,19 +19,17 @@ const Body = () => {
   // const { farmersDetailsById: farmersDetailsById, searchFilter, sortFilter, groupFilter } = useFarmerDetailsContext();
   const { searchFilter, sortFilter, groupFilter, currentPage } = useSelector((state: any) => state.farmerDetails);
   const dispatch = useDispatch();
-  const { formatChangeSuccess: isSuccess, result }: any = useFetch(ENDPOINTS.farmerDetails);
-  const { data: farmersDetailsById } = result;
+  const {
+    formatChangeSuccess: isSuccess,
+    result: { data: farmersDetailsById },
+  }: any = useFetch(ENDPOINTS.farmerDetails);
   const [farmersListGroup, setFarmersListGroup] = useState<farmerDetail[]>([]);
   const [farmersListSearch, setFarmersListSearch] = useState<farmerDetail[]>([]);
   const [farmersListSort, setFarmersListSort] = useState<farmerDetail[]>([]);
   const [farmersList, setFarmersList] = useState<farmerDetail[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    if (isSuccess && farmersList.length > 0) {
-      setIsLoading(true);
-    }
-  }, [farmersList, isSuccess]);
+  const [exportFarmerId, setExportFarmerID] = useState<farmerDetail[]>([]);
 
+  // farmer group filter for farmer detail table
   useEffect(() => {
     if (isSuccess) {
       setFarmersListGroup(
@@ -39,55 +44,48 @@ const Body = () => {
   useEffect(() => {
     // isSuccess && setFarmersListSearch(farmersListGroup.filter((farmer) => searchWord(farmer.name, searchFilter)));
     let result = isSuccess && Object.values(farmersListGroup as farmerDetail[]).filter((farmer) => searchWord(farmer.name, searchFilter));
+    setExportFarmerID(sortObj<farmerDetail>(Object.values(result), sortFilter, "name"));
     let updatedData = isSuccess && [...result];
     isSuccess && setFarmersListSearch(result.splice((currentPage - 1) * 25, 25));
     dispatch(setPageCount({ pageCount: Math.ceil(result.length / 25) + 1, totalPageCount: updatedData.length }));
-  }, [searchFilter, farmersListGroup, isSuccess]);
+  }, [searchFilter, farmersListGroup, isSuccess, sortFilter]);
 
   useEffect(() => {
     isSuccess && setFarmersListSort(sortObj<farmerDetail>(farmersListSearch, sortFilter, "name"));
   }, [farmersListSearch, sortFilter, isSuccess]);
 
+  //farmer id to export farmers
   useEffect(() => {
     isSuccess && setFarmersList(farmersListSort);
-    let farmersId = farmersListSort.map((item) => item.id);
+    let farmersId = exportFarmerId && exportFarmerId.map((item) => item.id);
     dispatch(setFarmersIdToExport(farmersId));
-  }, [farmersListSort, isSuccess]);
+  }, [farmersListSort, isSuccess, exportFarmerId]);
 
+  // For tamil share holder certificate
   useEffect(() => {
     if (isSuccess) {
       dispatch(checkBoxUnselectAll());
-      const farmerId = Object.values(isSuccess && farmersList).map((item: any) => item.id);
+      const farmerId = exportFarmerId && exportFarmerId.map((item: any) => item.id);
       isSuccess && dispatch(addFarmerId(farmerId));
     }
-  }, [isSuccess, farmersList]);
+  }, [isSuccess, farmersList, exportFarmerId]);
 
   return (
     <>
-      {isLoading ? (
-        <>
-          {farmersList.length > 0 ? (
-            <BodyWrapper>
-              {farmersList.map((user: farmerDetail) => (
-                <FarmersDetailsRow {...{ user }} key={user.id} />
-              ))}
-            </BodyWrapper>
-          ) : (
-            <S.EmptyMsg>
-              <tr>
-                <td>No Farmers Details..</td>
-              </tr>
-            </S.EmptyMsg>
-          )}
-        </>
+      {farmersList.length > 0 ? (
+        <BodyWrapper>
+          {farmersList.map((user: farmerDetail) => (
+            <FarmersDetailsRow {...{ user }} key={user.id} />
+          ))}
+        </BodyWrapper>
       ) : (
-        <tbody>
+        <S.EmptyMsg>
           <tr>
             <td>
               <Loader />
             </td>
           </tr>
-        </tbody>
+        </S.EmptyMsg>
       )}
     </>
   );
