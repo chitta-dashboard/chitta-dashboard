3ffd0e28e729058606ed45474a76bfef8f245cf1
adurhmn-aeq <abdur@aequalisys.com>
2022-11-08 11:01:34 +0530

    Toast message integrated in resolutions tab.

--- src/views/resolution-certificate/index.tsx
+++ src/views/resolution-certificate/index.tsx
@@ -12,6 +12,7 @@ import { useDelete, useEdit, useFetch } from "../../utils/hooks/query";
 import { ENDPOINTS, MessageStructured } from "../../utils/constants";
 import { useAuthContext } from "../../utils/context/auth";
 import Loader from "../../components/loader";
+import Toast from "../../utils/toast";
 import { S } from "./resolutionCertificate.styled";
 
 const ResolutionCertificatePage = () => {
@@ -49,6 +50,10 @@ const ResolutionCertificatePage = () => {
             id: "delete" + resolutionId,
             message: MessageStructured(resolutions[resolutionId as string].groupTitle, ENDPOINTS.resolutions, "delete"),
           });
+          Toast({ message: "Resolution deleted successfully.", type: "success" });
+        },
+        errorCb: () => {
+          Toast({ message: "Request failed, please try again.", type: "error" });
         },
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -144,6 +149,10 @@ const ResolutionCertificatePage = () => {
                   id: "edit" + resolutionId,
                   message: MessageStructured(resolutions[resolutionId as string].groupTitle, ENDPOINTS.resolutions, "edit"),
                 });
+                Toast({ message: "Resolution edited successfully.", type: "success" });
+              },
+              errorCb: () => {
+                Toast({ message: "Request failed, please try again.", type: "error" });
               },
             });
             setConfirmation(false);
