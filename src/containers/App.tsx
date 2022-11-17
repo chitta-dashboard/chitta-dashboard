56c646075c601a4fdbea747af2d973df41dcd027
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-11-17 11:36:42 +0530

    Products modal UI updated.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,17 +3,17 @@ import Layout from "./layout/Layout";
 import Provider from "./provider";
 import Login from "../views/login";
 import { ToastContainer } from "../utils/toast";
-import ModalLaunchButtons from "../components/modals/ModalLaunchButtons";
+// import ModalLaunchButtons from "../components/modals/ModalLaunchButtons";
 
 const App = () => {
   return (
     <Provider>
-      {/* <Routes>
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Layout />} />
       </Routes>
-      <ToastContainer newestOnTop hideProgressBar={true} pauseOnFocusLoss={false} /> */}
-      <ModalLaunchButtons />
+      <ToastContainer newestOnTop hideProgressBar={true} pauseOnFocusLoss={false} />
+      {/* <ModalLaunchButtons /> */}
     </Provider>
   );
 };
