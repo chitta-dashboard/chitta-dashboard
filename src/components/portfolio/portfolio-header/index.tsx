a632bd7b6f24dc7836c0b96ea738f94a4b85cf06
GOWTHAMRAJASEKARAN97 <gowthamrajasekaran97@gmail.com>
2022-11-21 10:37:04 +0530

    Products modal integrated on portfolio page.

--- src/components/portfolio/portfolio-header/index.tsx
+++ src/components/portfolio/portfolio-header/index.tsx
@@ -3,11 +3,32 @@ import SearchBar from "../../common-components/search-bar";
 import { Button } from "@mui/material";
 import ToggleButton from "../../../utils/ToggleButton";
 import ProductsModal from "../../modals/products-modal";
+import { IAddProductsFormInput } from "../../modals/type/formInputs";
+import { useAdd } from "../../../utils/hooks/query";
+import { ENDPOINTS, Endpoints, Message } from "../../../utils/constants";
+import { useAuthContext } from "../../../utils/context/auth";
 import S from "./portfolioHeader.styled";
+import Toast from "../../../utils/toast";
 
 const PortfolioHeader: FC = () => {
   const [tab, setTab] = useState("Raw");
   const [addModalOpen, setAddModalOpen] = useState(false);
+
+  const { mutate: porfolioAdd } = useAdd(ENDPOINTS.portfolioRaw as Endpoints);
+  const { addNotification } = useAuthContext();
+
+  const addDataHandler = (data: IAddProductsFormInput & { id: string }) => {
+    porfolioAdd({
+      data: data,
+      successCb: () => {
+        Toast({ message: "Product added successfully.", type: "success" });
+      },
+      errorCb: () => {
+        Toast({ message: "Request failed, please try again.", type: "error" });
+      },
+    });
+    addNotification({ id: data.id, image: data.profile, message: Message(data.name).addProduct });
+  };
 
   return (
     <>
@@ -19,10 +40,10 @@ const PortfolioHeader: FC = () => {
         <ToggleButton selectedMode={tab} setSelectedMode={setTab} options={["Raw", "Processed", "Animal"]} />
         <S.Action>
           <SearchBar />
-          <Button onClick={() => {}}>Add</Button>
+          <Button onClick={() => setAddModalOpen(true)}>Add</Button>
         </S.Action>
       </S.Header>
-      {addModalOpen && <ProductsModal openModal={true} handleClose={() => setAddModalOpen(false)} cb={(data) => {}} />}
+      {addModalOpen && <ProductsModal openModal={true} handleClose={() => setAddModalOpen(false)} cb={addDataHandler} />}
     </>
   );
 };
