4f3bfb3ae409a821807a135a73ad1b600be3cbb8
Gandhi-Rajendran <ksrmgandhiraj@gmail.com>
2022-11-23 19:33:22 +0530

    Resolved Delete and Clear notification issue.

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -5,9 +5,9 @@ import S from "./icon-styled";
 const Icon: FC<{
   iconName: IconType;
   clickHandler?: () => void;
-  color?:boolean
-}> = ({ iconName, clickHandler,color }) => {
-  return <S.Icon isColor={color? 1:0} onClick={clickHandler} className={`nerkathir-icon-${iconName}`}></S.Icon>;
+  color?: boolean;
+}> = ({ iconName, clickHandler, color }) => {
+  return <S.Icon isColor={color ? 1 : 0} onClick={clickHandler} className={`nerkathir-icon-${iconName}`}></S.Icon>;
 };
 
 export default Icon;
