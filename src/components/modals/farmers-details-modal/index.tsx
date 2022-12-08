2212d463635639a5c1329cc22cb67ab3b8231ddc
raviprasad-aeq <raviprasad@aequalisys.com>
2022-12-08 19:07:44 +0530

    Changed all landreaincent value to number from string, landareato cent data will store as number in db,  type error fixed for land area in cent.

--- src/components/modals/farmers-details-modal/index.tsx
+++ src/components/modals/farmers-details-modal/index.tsx
@@ -277,12 +277,13 @@ const FarmersDetailsModalHandler: FC<CustomProps> = (props) => {
       id: mdId ? mdId : editMode ? id : newId,
       membershipId: id && editMode ? farmersDetailsById[id].membershipId : `NER-FPC-${newMemberId}`,
       farmerId: id,
-      landAreaInCent: `${Object.values(form1Data?.acre as IAddFarmersDetailsPage1Input).reduce((a, b) => {
-        return a + parseInt(b as string);
-      }, 0) * ACRETOCENT}`,
+      landAreaInCent:
+        Object.values(form1Data?.acre as IAddFarmersDetailsPage1Input).reduce((a, b) => {
+          return a + parseInt(b as string);
+        }, 0) * ACRETOCENT,
     } as IAddFarmersDetailsPage1Input &
       IAddFarmersDetailsPage2Input &
-      IAddFarmersDetailsPage3Input & { id: string; membershipId: string | undefined; farmerId?: string; landAreaInCent: string };
+      IAddFarmersDetailsPage3Input & { id: string; membershipId: string | undefined; farmerId?: string; landAreaInCent: number };
     cb({ ...params } as IAddFarmersDetailsFormInput & { id: string; membershipId: string; farmerId?: string });
     !editMode && handleClose();
     //console.log("Params : ",params)
