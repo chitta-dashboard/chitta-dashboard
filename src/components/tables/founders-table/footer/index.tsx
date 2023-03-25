3559db6481611ac786f32ede8b418ecd6ef1e8db
raviprasad-aeq <raviprasad@aequalisys.com>
2023-03-25 15:16:02 +0530

    Context of Md details, founder and portfolio checked and errors are fixed.

--- src/components/tables/founders-table/footer/index.tsx
+++ src/components/tables/founders-table/footer/index.tsx
@@ -1,11 +1,16 @@
-import { useFounderContext } from "../../../../utils/context/founders";
+import { ENDPOINTS } from "../../../../utils/constants";
+import { useFetch } from "../../../../utils/hooks/query";
 import FooterWrapper from "../../../custom-tables/footer";
 
 const Footer = () => {
-  const { foundersById } = useFounderContext();
-  const count = Math.ceil(Object.values(foundersById).length / 6);
+  const {
+    formatChangeSuccess,
+    result: { data: foundersById },
+  } = useFetch(ENDPOINTS.founders);
 
-  return Object.values(foundersById).length > 0 ? (
+  const count = Math.ceil(Object.values(formatChangeSuccess && foundersById).length / 6);
+
+  return Object.values(formatChangeSuccess && foundersById).length > 0 ? (
     <FooterWrapper count={count} page={1} totalCount={Object.values(foundersById).length} rowsPerPage={6} />
   ) : null;
 };
--- src/utils/context/founders.tsx
+++ src/utils/context/founders.tsx
@@ -2,9 +2,6 @@ import { createContext, FC, useContext, useReducer } from "react";
 import { NORMAL, SortOrder } from "../constants";
 
 //ACTION TYPES
-const ADD_FOUNDERS = "ADD_FOUNDERS";
-const EDIT_FOUNDERS = "EDIT_FOUNDERS";
-const DELETE_FOUNDERS = "DELETE_FOUNDERS";
 const SET_SEARCH_FILTER = "SET_SEARCH_FILTER";
 const SET_SORT_FILTER = "SET_SORT_FILTER";
 
@@ -24,100 +21,21 @@ type Props = {
 };
 
 export interface FoundersContextType {
-  foundersById: { [id: string]: Founders };
   searchFilter: string;
   sortFilter: SortOrder;
   setSortFilter: (sortOrder: SortOrder) => void;
   setSearchFilter: (searchText: string) => void;
-  addFounder: (data: Founders) => void;
-  editFounder: (data: Founders) => void;
-  deleteFounder: (id: string) => void;
 }
 
 const initialState: FoundersContextType = {
-  foundersById: {
-    a: {
-      id: "a",
-      profile: "",
-      name: "Veera Raghavan",
-      phoneNumber: "9945672156",
-      qualification: "BBA, MBA",
-      dob: "01-10-1982",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-    b: {
-      id: "b",
-      profile: "",
-      name: "John Durairaj",
-      phoneNumber: "8610010875",
-      qualification: "BA",
-      dob: "27-01-1990",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-    c: {
-      id: "c",
-      profile: "",
-      name: "Vijay Kumar",
-      phoneNumber: "8968456734",
-      qualification: "BCom CA",
-      dob: "09-11-1989",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-    d: {
-      id: "d",
-      profile: "",
-      name: "Kathiresan",
-      phoneNumber: "8838461839",
-      qualification: "BSc, Computer Science",
-      dob: "12-10-1994",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-    e: {
-      id: "e",
-      profile: "",
-      name: "Jeevanandham",
-      phoneNumber: "9854367213",
-      qualification: "B.Tech, Information Technology",
-      dob: "02-08-1992",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-    f: {
-      id: "f",
-      profile: "",
-      name: "Arockiyaraj Reddy",
-      phoneNumber: "9945672156",
-      qualification: "B.Tech, Computer Science",
-      dob: "12-07-1985",
-      description: "Cultivates land",
-      joinDate: "mar 16,2022",
-    },
-  },
   searchFilter: "",
   sortFilter: NORMAL,
   setSortFilter: () => {},
   setSearchFilter: () => {},
-  addFounder: () => {},
-  editFounder: () => {},
-  deleteFounder: () => {},
 };
 
 const reducer = (state: FoundersContextType, action: any) => {
   switch (action.type) {
-    case ADD_FOUNDERS:
-      return { ...state, foundersById: { [action.payload.id]: action.payload, ...state.foundersById } };
-
-    case EDIT_FOUNDERS:
-      return { ...state, foundersById: { ...state.foundersById, [action.payload.id]: action.payload } };
-
-    case DELETE_FOUNDERS:
-      delete state.foundersById[action.payload];
-      return { ...state, foundersById: { ...state.foundersById } };
-
     case SET_SEARCH_FILTER:
       return { ...state, searchFilter: action.payload };
 
@@ -135,18 +53,6 @@ export const foundersContext = createContext<FoundersContextType>(initialState);
 const FoundersContextProvider: FC<Props> = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const addFounder = (data: Founders) => {
-    dispatch({ type: ADD_FOUNDERS, payload: data });
-  };
-
-  const editFounder = (data: Founders) => {
-    dispatch({ type: EDIT_FOUNDERS, payload: data });
-  };
-
-  const deleteFounder = (id: string) => {
-    dispatch({ type: DELETE_FOUNDERS, payload: id });
-  };
-
   const setSearchFilter = (searchText: string) => {
     dispatch({ type: SET_SEARCH_FILTER, payload: searchText });
   };
@@ -157,9 +63,6 @@ const FoundersContextProvider: FC<Props> = (props) => {
 
   let data = {
     ...state,
-    addFounder,
-    editFounder,
-    deleteFounder,
     setSearchFilter,
     setSortFilter,
   };
